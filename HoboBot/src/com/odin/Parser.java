c7e247d160e3ceccb5c8ae3a5be735a6b8fb84b0
bobothehobo2 <brian.voss@verizon.net>
2017-02-03 14:18:39 -0600

    initial upload

---
+++ HoboBot/src/com/odin/Parser.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.odin;
+
+/**
+ * @author Brian
+ *
+ */
+public class Parser {
+
+}
---
+++ HoboBot/src/com/odin/main/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.odin.main;
+
+/**
+ * @author Brian
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
