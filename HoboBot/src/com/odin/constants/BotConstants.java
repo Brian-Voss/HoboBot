950e2836ec310d7d3e6f784500237018519adc44
bobothehobo2 <brian.voss@verizon.net>
2017-02-09 15:56:18 -0600

    Framework Update
    Adding initial listners and some parsing code

---
+++ HoboBot/src/com/odin/constants/BotConstants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.odin.constants;
+
+/**
+ * @author Brian
+ *
+ */
+public class BotConstants {
+	
+	public static final String BOT_TOKEN = "Mjc5Mjc0NjEzMzAzNjcyODMz.C34f4w.SI0nVfQffzLEmtJHyVzJttzskvo";
+
+}
---
+++ HoboBot/src/com/odin/listeners/ReadyListener.java
@@ -0,0 +1,12 @@
+package com.odin.listeners;
+
+import net.dv8tion.jda.core.events.ReadyEvent;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+public class ReadyListener extends ListenerAdapter {
+
+	@Override
+	public void onReady(ReadyEvent event) {
+		System.out.println("Hobo wanders into your discord");
+	}
+}
